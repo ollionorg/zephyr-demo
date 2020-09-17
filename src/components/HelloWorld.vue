6b7026589723cf28978869928fa3090b31f259ee
Roshan Patil <patilroshan443@gmail.com>
2020-09-17 13:57:18 +0530

    Initial Zephyr demo setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a href="https://beta.cldcvr.app" target="_blank">Deploy Here</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      msg: "Welcome to Zephyr Project",
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+
+provider "google-beta" {
+}
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "random" {}
+
+resource "random_string" "random" {
+  length    = 8
+  special   = false
+  min_lower = 8
+}
+
+variable project_id {
+  default = "vanguard-test-deploy"
+}
+
+resource "google_storage_bucket" "website_bucket" {
+  provider      = google-beta
+  project       = var.project_id
+  name          = "zephyr-demo-${random_string.random.result}"
+  force_destroy = true
+
+  website {
+    main_page_suffix = "output.html"
+    not_found_page   = "output.html"
+  }
+
+  lifecycle_rule {
+    condition {
+      age = "1"
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
+resource "google_storage_bucket_acl" "website_bucket_acl" {
+  provider    = google-beta
+  bucket      = google_storage_bucket.website_bucket.name
+  role_entity = ["READER:allUsers"]
+}
+
