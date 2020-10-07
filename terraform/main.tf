579e0c6d12647827c9df2f85e1a5761c24301108
nadzir <nadzir@cldcvr.com>
2020-10-07 19:16:19 +0800

    change provider to google-beta

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,11 +44,10 @@ resource "google_storage_bucket_acl" "website_bucket_acl" {
 }
 
 resource "google_storage_default_object_access_control" "public_rule" {
+  provider    = google-beta
   bucket = google_storage_bucket.website_bucket.name
   role   = "READER"
   entity = "allUsers"
-
-  depends_on = [google_storage_bucket_acl.website_bucket_acl]
 }
 
 
